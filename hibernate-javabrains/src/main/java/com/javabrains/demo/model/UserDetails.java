68d5b2100006f5003b6c975a5e27c23fad960df6
lazaros-batsilas <lbatsilas7@gmail.com>
2021-11-30 18:50:50 +0200

    Add test for saving user details

--- hibernate-javabrains/src/main/java/com/javabrains/demo/model/UserDetails.java
+++ hibernate-javabrains/src/main/java/com/javabrains/demo/model/UserDetails.java
@@ -3,10 +3,12 @@ package com.javabrains.demo.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
+@AllArgsConstructor
 public class UserDetails {
 
 	@Id
--- hibernate-javabrains/src/test/java/com/javabrains/demo/HibernateJavabrainsApplicationTests.java
+++ hibernate-javabrains/src/test/java/com/javabrains/demo/HibernateJavabrainsApplicationTests.java
@@ -1,13 +1,34 @@
 package com.javabrains.demo;
 
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.javabrains.demo.model.UserDetails;
 
 @SpringBootTest
 class HibernateJavabrainsApplicationTests {
+	
+	@Autowired
+	private EntityManagerFactory entityManagerFactory;
 
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void saveUserDetails() {
+		SessionFactory sessionFactory = 
+					this.entityManagerFactory.unwrap(SessionFactory.class);
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(new UserDetails(1, "First user"));
+		session.getTransaction().commit();
+	}
+	
 
 }
