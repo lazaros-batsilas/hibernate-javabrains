0b1643cc2c48460f526cb32eb01d1eb19020abda
lazaros-batsilas <lbatsilas7@gmail.com>
2021-11-30 19:04:03 +0200

    Customize user details table

--- hibernate-javabrains/src/main/java/com/javabrains/demo/model/UserDetails.java
+++ hibernate-javabrains/src/main/java/com/javabrains/demo/model/UserDetails.java
@@ -1,17 +1,20 @@
 package com.javabrains.demo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Entity
+@Entity	(name="USER_DETAILS")
 @Data
 @AllArgsConstructor
 public class UserDetails {
 
 	@Id
+	@Column(name="USER_ID")
 	private int userId;
+	@Column(name="USER_NAME")
 	private String username;
 }
