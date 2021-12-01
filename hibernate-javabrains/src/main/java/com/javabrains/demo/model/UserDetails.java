95c0755231925bcb15682fa139aa38b728e66b3f
lazaros-batsilas <lbatsilas7@gmail.com>
2021-12-01 13:23:38 +0200

    generate primary key for address table - use @ElementCollection annotation

--- hibernate-javabrains/src/main/java/com/javabrains/demo/model/UserDetails.java
+++ hibernate-javabrains/src/main/java/com/javabrains/demo/model/UserDetails.java
@@ -1,16 +1,22 @@
 package com.javabrains.demo.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,16 +29,23 @@ import lombok.NoArgsConstructor;
 @Table(name="USER_DETAILS")
 public class UserDetails {
 
-	@Id @GeneratedValue
+	@Id 	
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int userId;
 	private String username;
 	@Temporal(TemporalType.DATE)
 	private Date dateJoined;
 	@ElementCollection
-	private Set<Address> addresses= new HashSet<Address>();
+	@GenericGenerator(name="myGenerator",strategy="sequence")
+	@CollectionId(
+	    columns=@Column(name="address"),
+	    type=@Type(type="long"),
+	    generator="myGenerator"
+	)
+	private Collection<Address> addresses= new ArrayList<Address>();
 	private String description;
 	
-	public UserDetails(String username, Date dateJoined, Set<Address> addresses, String description) {
+	public UserDetails(String username, Date dateJoined, Collection<Address> addresses, String description) {
 		this.username = username;
 		this.dateJoined = dateJoined;
 		this.addresses = addresses;
